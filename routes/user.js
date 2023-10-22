1bef08753911979f946a1e717a4c9d6af9a9d5af
samped <michaelsamuelpedro@gmail.com>
2023-10-22 16:16:02 +0100

    feature/signup

--- routes/user.js
+++ routes/user.js
@@ -1,55 +1,59 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt"); //* bcrypt is a 3rd party library which we use to hash, salt and compare passwords
 const User = require("../model/userModel");
-const bcrypt = require("bcrypt");
 
 const {
-    validateName,
-    validateEmail,
-    validatePassword
+  validateName,
+  validateEmail,
+  validatePassword,
 } = require("../utils/validators");
 
 router.post("/signup", async (req, res) => {
-    try {
+  try {
+    // destructuring name, email and password out of the request body
+    const { name, email, password, isSeller } = req.body; 
 
-        const { name, email, password, isSeller } = req.body;
-
-        //checking if user exists in the databass
-        const existingUser = await User.findOne({ where: {email}});
-        if(existingUser) {
-            return res.status(403).json({ err: "User already exists"});
-        }
-
-        //validating the user inputs (name, email, password)
-        if(!validateName(name)) {
-            return res.status(400).json({err: "Name must be at least 3 character long and must not include numbers or special characters"});
-        }
-        if(!validateEmail(email)) {
-            return res.status(400).json({err: "Invalide Email address"});
-        }
-        if(!validatePassword(password)) {
-            return res.status(400).json({err: "Password must be atleast 8 character long and it must include atleast - one uppercase letter, one lowercase letter, one digit, one special character"});
-        }
-
-        const hashPassword = await bcrypt.hash(password);
-
-        const user = {
-            email,
-            name,
-            isSeller,
-            password: hashPassword
-        };
-
-        const createdUser = await User.create(user);
-
-        return res.status(201).json({
-            message: `Welcome ${createdUser.name}`
-        })
-
-    } catch (e) {
-        return res.status(500).send(e);
+    //* check if the user with the entered email already exists in the database
+    const existingUser = await User.findOne({ where: { email} }); 
+    if (existingUser) {
+      return res.status(403).json({ err: "User already exists" });
+    }
+    //validates all user inputs (name, email, password, isSeller||not)
+    if (!validateName(name)) {
+      return res.status(400).json({
+        err: "Invalid user name: name must be longer than two characters and must not include any numbers or special characters",
+      });
     }
 
+    if (!validateEmail(email)) {
+      return res.status(400).json({ err: "Error: Invalid email" });
+    }
+
+    if (!validatePassword(password)) {
+      return res.status(400).json({
+        err: "Error: Invalid password: password must be at least 8 characters long and must include atleast one - one uppercase letter, one lowercase letter, one digit, one special character",
+      });
+    }
+    //hashes password with saltorrounds
+    const hashedPassword = await bcrypt.hash(password, (saltOrRounds = 10)); 
+
+    const user = {
+      email,
+      name,
+      password: hashedPassword,
+      isSeller: false,
+    };
+    //creates the user
+    const createdUser = await User.create(user);
+
+    return res.status(201).json({
+      message: `Welcome to Devsnest ${createdUser.name}. Thank you for signing up`,
+    });
+  } catch (err) {
+    return res.status(500).json({ err: err.message });
+  }
 });
+
 
 module.exports = router;
